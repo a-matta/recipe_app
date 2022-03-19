f2992c6e8848b5b97390ebeee57847c0d1c4f56c
Amrita Matta <matta.amrita@gmail.com>
2022-03-19 22:07:14 +0200

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import RecipeCard from "./components/RecipeCard";
+import SearchBar from "./components/SearchBar";
 
+const API = " https://www.themealdb.com/api/json/v1/1/search.php?s=";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Recipe App</h2>
+      <SearchBar />
+      <RecipeCard />
     </div>
   );
 }
---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecipeCard = (props) => {
+  return <div>RecipeCard</div>;
+};
+
+export default RecipeCard;
