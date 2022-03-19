2a470c9ce3e4e624b4ea739e1c2307126546f20d
Amrita Matta <matta.amrita@gmail.com>
2022-03-19 22:21:25 +0200

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import RecipeCard from "./components/RecipeCard";
 import SearchBar from "./components/SearchBar";
 
 const API = " https://www.themealdb.com/api/json/v1/1/search.php?s=";
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setIsquery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const searchRecipes = async () => {
+    setIsLoading = true;
+    const url = API + query;
+    const res = await fetch(url);
+    const data = res.json();
+    console.log(data);
+  };
+
   return (
     <div className="container">
       <h2>Recipe App</h2>
